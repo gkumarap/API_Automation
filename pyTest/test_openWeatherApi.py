a3adbc6c64f1f2182e7814b17244866d3b3ab87b
govind <govindec15@gmail.com>
2020-04-19 20:12:25 +0530

    patch a sample get request with parameters and more to come

---
+++ pyTest/test_openWeatherApi.py
@@ -0,0 +1,13 @@
+import requests as REQ
+
+
+appid = "673c5650a20311041c26d61291b186ae"
+id = "2172797"
+Base_url = "http://api.openweathermap.org/data/2.5/weather"
+
+params ={"appid":appid,'id':id}
+
+response= REQ.get(Base_url,params=params)
+
+print(response.request)
+print(response.text)
